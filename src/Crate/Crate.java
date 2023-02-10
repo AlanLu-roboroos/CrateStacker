68b46a1247a438c761edae51913f95c9031c44ac
AlanLu <alan.lu@roboroos.org.au>
2023-02-11 00:49:03 +1030

    Created base classes for Grabber, Crate and Items

---
+++ src/Crate/Crate.java
@@ -0,0 +1,24 @@
+package Crate;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public abstract class Crate extends Canvas {
+  private int line;
+  private int height;
+  private boolean infected;
+  private boolean explosionResistence;
+  private Image image;
+
+  public Crate(int line, int height) {
+    this.line = line;
+    this.height = height;
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+  }
+}
