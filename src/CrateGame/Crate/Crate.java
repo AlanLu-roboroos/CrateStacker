79b1d522e45aaa15585fe47943c05404feeaf8fa
AlanLu <alan.lu@roboroos.org.au>
2023-02-11 19:47:14 +1030

    Change crate to an interface

--- src/CrateGame/Crate/Crate.java
+++ src/CrateGame/Crate/Crate.java
@@ -4,22 +4,6 @@ import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Image;
 
-public abstract class Crate extends Canvas {
-  private int line;
-  private int height;
-  private boolean infected;
-  private boolean explosionResistence;
-  private Image image;
-
-  private int[] pos;
-
-  public Crate(int line, int height) {
-    this.line = line;
-    this.height = height;
-  }
-
-  @Override
-  public void paint(Graphics g) {
-    super.paint(g);
-  }
+public interface Crate {
+  public void paint(Graphics g);
 }
