3e624d39e28d30fbb95d02a72c07a0ed24143e36
AlanLu <alan.lu@roboroos.org.au>
2023-02-15 07:22:29 +1030

    Continue working on Grabber

--- src/CrateGame/Grabber.java
+++ src/CrateGame/Grabber.java
@@ -8,96 +8,104 @@ import CrateGame.Crate.Crate;
 import java.awt.Graphics;
 
 public class Grabber {
-  public int x, y;
-  public Image Left, Right, Horizontal, Vertical;
+    public int x, y;
+    public Image Left, Right, Horizontal, Vertical;
 
-  public long startTime;
+    public long startTime;
 
-  public Crate heldCrate = null;
+    public Crate heldCrate = null;
 
-  public ArrayList<ArrayList<Crate>> crates;
+    public ArrayList<ArrayList<Crate>> crates;
 
-  public enum State {
-    STANDBY,
-    RAISING,
-    LOWERING
-  }
-
-  public State state = State.STANDBY;
-
-  public double currColumn = 3.5;
-  public int currHeight = -1;
-
-  public Grabber(ArrayList<ArrayList<Crate>> crates) {
-    x = Constants.WIDTH / 2;
-    y = Constants.BORDER_HEIGHT;
-
-    this.crates = crates;
-
-    Left = Constants.Images.GRABBER_LEFT_IMG;
-    Right = Constants.Images.GRABBER_RIGHT_IMG;
-    Horizontal = Constants.Images.GRABBER_HORIZONTAL_IMG;
-    Vertical = Constants.Images.GRABBER_VERTICAL_IMG;
-
-    startTime = 0;
-  }
-
-  public void goTo(int column) {
-    if (state == State.STANDBY) {
-        currColumn = column;
-        currHeight = crates.get(column).size();
-        state = State.LOWERING;
+    public enum State {
+        STANDBY,
+        RAISING,
+        LOWERING
     }
-  }
 
-  public void paint(Graphics g) {
-    if (state == State.STANDBY) {
+    public State state = State.STANDBY;
+
+    public double currColumn = 3.5;
+    public int currHeight = -1;
+
+    public Grabber(ArrayList<ArrayList<Crate>> crates) {
+        x = Constants.WIDTH / 2;
         y = Constants.BORDER_HEIGHT;
-    } else if (state == State.RAISING) {
-        if (y < Constants.BORDER_HEIGHT) {
-            state = State.STANDBY;
-        } else {
-            y -= Constants.GRABBER_SPEED;
+
+        this.crates = crates;
+
+        Left = Constants.Images.GRABBER_LEFT_IMG;
+        Right = Constants.Images.GRABBER_RIGHT_IMG;
+        Horizontal = Constants.Images.GRABBER_HORIZONTAL_IMG;
+        Vertical = Constants.Images.GRABBER_VERTICAL_IMG;
+
+        startTime = 0;
+    }
+
+    public void goTo(int column) {
+        if (state == State.STANDBY) {
+            currColumn = column;
+            currHeight = crates.get(column).size();
+            state = State.LOWERING;
+            startTime = System.currentTimeMillis();
         }
-    } else if (state == State.LOWERING) {
-        if (heldCrate == null && y > currHeight || heldCrate != null && y > heldCrate.getY()) {
-            state = State.RAISING;
-            int crateHeight = crates.get((int)currColumn).size() - 1;
-            if (heldCrate == null) {
-                if (crateHeight >= 0) {
-                    if (heldCrate == null) {
-                        heldCrate = crates.get((int)currColumn).get(crateHeight);
-                        crates.get((int)currColumn).remove(crateHeight);
+    }
+
+    public void paint(Graphics g) {
+        if (state == State.STANDBY) {
+            y = Constants.BORDER_HEIGHT;
+        } else if (state == State.RAISING) {
+            if (y < Constants.BORDER_HEIGHT) {
+                state = State.STANDBY;
+            } else {
+                y = (int) (Constants.PLATFORM_POS[0][1] - Constants.Images.CRATE_HEIGHT * currHeight
+                        - Constants.GRABBER_SPEED * (System.currentTimeMillis() - startTime) / 1000);
+            }
+        } else if (state == State.LOWERING) {
+            if (heldCrate == null && crates.get((int) currColumn).size() > 0 && y > Constants.PLATFORM_POS[0][1]
+                    || heldCrate == null && crates.get((int) currColumn).size() > 0
+                            && y > crates.get((int) currColumn).get(crates.get((int) currColumn).size() - 1).getY()
+                                    - Constants.Images.CRATE_HEIGHT - 70
+                    || heldCrate != null && y > heldCrate.getY() - Constants.Images.CRATE_HEIGHT - 70) {
+                state = State.RAISING;
+                startTime = System.currentTimeMillis();
+                int crateHeight = crates.get((int) currColumn).size() - 1;
+                if (heldCrate == null) {
+                    if (crateHeight >= 0) {
+                        if (heldCrate == null) {
+                            heldCrate = crates.get((int) currColumn).get(crateHeight);
+                            crates.get((int) currColumn).remove(crateHeight);
+                        }
+                    }
+                } else {
+                    if (crates.get((int) currColumn).size() < 6) {
+                        heldCrate.setColumn((int) currColumn);
+                        heldCrate.setHeight(currHeight);
+                        crates.get((int) currColumn).add(heldCrate);
+                        heldCrate = null;
                     }
                 }
             } else {
-                if (crates.get((int)currColumn).size() < 6) {
-                    heldCrate.setColumn((int)currColumn);
-                    heldCrate.setHeight(currHeight);
-                    crates.get((int)currColumn).add(heldCrate);
-                    heldCrate = null;
-                }
+                y = (int) (Constants.BORDER_HEIGHT
+                        + Constants.GRABBER_SPEED * (System.currentTimeMillis() - startTime) / 1000);
             }
+        }
+
+        x = (int) (currColumn * 143 + 100);
+        g.drawImage(Vertical.getScaledInstance(80, Math.max(y - Constants.BORDER_HEIGHT + 30, 30), Image.SCALE_DEFAULT),
+                x - 40, Constants.BORDER_HEIGHT, null);
+        if (heldCrate == null) {
+            g.drawImage(Horizontal.getScaledInstance(100, 80, Image.SCALE_DEFAULT), x - 50, y + 30, null);
+            g.drawImage(Left, x - Constants.Images.CRATE_WIDTH / 2 - 85, y + 21, null);
+            g.drawImage(Right, x - Constants.Images.CRATE_WIDTH / 2 + 85, y + 21, null);
         } else {
-            y += Constants.GRABBER_SPEED;
+            g.drawImage(Horizontal, x - 35, y + 30, null);
+            g.drawImage(Left, x - Constants.Images.CRATE_WIDTH / 2 - 70, y + 21, null);
+            g.drawImage(Right, x - Constants.Images.CRATE_WIDTH / 2 + 70, y + 21, null);
+        }
+        if (heldCrate != null) {
+            g.drawImage(heldCrate.getImage(), x - Constants.Images.CRATE_WIDTH / 2, y + 45, null);
         }
     }
-
-
-    x = (int) (currColumn * 143 + 100);
-    g.drawImage(Vertical.getScaledInstance(80, Math.max(y - Constants.BORDER_HEIGHT + 30, 30), Image.SCALE_DEFAULT), x - 40, y, null);
-    if (heldCrate == null) {
-        g.drawImage(Horizontal.getScaledInstance(100, 80, Image.SCALE_DEFAULT), x - 50, y + 30, null);
-        g.drawImage(Left, x - Constants.Images.CRATE_WIDTH / 2 - 85, y + 21, null);
-        g.drawImage(Right, x - Constants.Images.CRATE_WIDTH / 2 + 85, y + 21, null);
-    } else {
-        g.drawImage(Horizontal, x - 35, y + 30, null);
-        g.drawImage(Left, x - Constants.Images.CRATE_WIDTH / 2 - 70, y + 21, null);
-        g.drawImage(Right, x - Constants.Images.CRATE_WIDTH / 2 + 70, y + 21, null);
-    }
-    if (heldCrate != null) {
-        g.drawImage(heldCrate.getImage(), x- Constants.Images.CRATE_WIDTH / 2, y + 45, null);
-    }
-  }
 
 }
